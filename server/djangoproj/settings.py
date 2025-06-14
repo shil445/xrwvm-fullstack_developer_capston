7bbaca86e119e88d72a45927cc7ce056b8291c8b
Phyo-Alpha <phyyyopyaesome@gmail.com>
2025-06-14 19:04:45 +0700

    Update .gitignore, remove test.txt, add new images and contact page, and enhance About page content

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,8 +27,13 @@ SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://phyyyopyaeso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://phyyyopyaeso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "https://phyyyopyaeso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://phyyyopyaeso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/"
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
