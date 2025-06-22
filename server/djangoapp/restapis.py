ff7ec118db9b1a709828d327019d9b464a15c674
Phyo-Alpha <phyyyopyaesome@gmail.com>
2025-06-22 11:44:28 +0700

    Add new images for sentiment analysis and dealership reviews; update backend URL in .env for consistency and improve error handling in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -24,13 +24,14 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
+        print(e)
         # If any error occurs
         print("Network exception occurred")
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -38,14 +39,19 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
+
 # Add code for retrieving sentiments
 
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
         print("Network exception occurred")
+
+
 # Add code for posting review
