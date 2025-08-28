4da4f6645f96ae1777f7e7f54b79b68efef1c9fe
shil445 <shilpag@skillup.online>
2025-08-28 07:44:19 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -32,5 +32,9 @@ urlpatterns = [
         name="dealer_details",
     ),
     # path for add a review view
-    path(route="add_review", view=views.add_review, name="add_review"),
+    path(
+        route="add_review", 
+        view=views.add_review, 
+        name="add_review"
+        ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
